9014dcbda704ac00edc68077dcf887db82ee0410
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2021-08-28 00:07:36 -0300

    edicion de productos

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,24 +1,26 @@
-const express=require('express');
+const express = require('express');
 
-const router =express.Router();
+const router = express.Router();
 
-const mainController=require('../Controllers/mainControllers');
+const mainController = require('../Controllers/mainControllers');
 
 router.get('/', mainController.index)
 
-router.get('/admin', mainController.admin);
+router.get('/admin-carga', mainController.admin);
 
-router.get ('/categorias', mainController.categorias);
+router.get('/categorias', mainController.categorias);
 
-router.get ('/config-perfil', mainController.perfil);
+router.get('/config-perfil', mainController.perfil);
 
-router.get ('/formas-de-pago', mainController.pago);
+router.get('/formas-de-pago', mainController.pago);
 
-router.get ('/productCart', mainController.productCart);
+router.get('/productCart', mainController.productCart);
 
-router.get ('/productDetail', mainController.productDetail);
+router.get('/productDetail', mainController.productDetail);
 
-router.get ('/register', mainController.register);
+router.get('/register', mainController.register);
+
+router.get('/admin-edit', mainController.edit);
 
 
-module.exports=router 
+module.exports = router
