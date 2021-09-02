e294e99f6615e998d0a6af56e3091b20d64490cc
LautaroJuarez <jlautaro777@gmail.com>
2021-09-01 21:47:33 -0300

    últimos cambios

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,14 +1,8 @@
 let express = require('express')
-
 let router = express.Router()
 
-
-
 let { detail } = require('../controllers/productsController');
-
-
-
-
+//detalle del producto
 router.get('/detail/:id', detail)
 
 module.exports = router;
