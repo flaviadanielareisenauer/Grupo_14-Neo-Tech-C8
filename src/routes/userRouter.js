c07a7a556750cc8fc19b45427a0dae59e60d93ab
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2021-09-12 23:48:17 -0300

    userControllers

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,26 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 const { register,
-    perfil,
-    /*processRegister, */
+    profile,
+    profileEdit,
+    processRegister, 
     categorias,
     productCart, } = require('../controllers/userController')
 
 const registerValidator = require('../validations/registerValidator')
-//const userAvatars = require('../middlewares/userAvatars')
+const userAvatars = require('../middlewares/userAvatars')
 
 /* GET - REGISTER */
 router.get('/register', register)
-/*router.post('/register', registerValidator, processRegister);*/
+router.post('/register', registerValidator, processRegister);
 
 /* GET - LOGIN */
 
 
 /* GET - USER PROFILE */
-router.get('/config-perfil', perfil)
-
+router.get('/profile', profile)
+router.put('/profile-edit', userAvatars.single('avatar'), profileEdit)
 
 router.get('/categorias', categorias)
+
 router.get('/productCart', productCart)
 
 
