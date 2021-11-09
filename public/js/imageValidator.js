19b406a5e041e1d2c15c18477f2e36d62d0ed9e0
Daiana <Daiana.g.97@hotmail.com>
2021-11-09 09:15:45 -0300

    detalles  validaciones

--- public/js/imageValidator.js
+++ public/js/imageValidator.js
@@ -19,28 +19,3 @@ $file.addEventListener('change',
     })
   })
 
-  /*let file = document.getElementById('cambiarImagen')
-
-file.addEventListener('change', 
-function fileValidation(){
-    var fileError = document.getElementById('fileErrors')
-    var filePath = file.value; //Capturo el valor del input
-    var allowefExtensions = /(.jpg|.jpeg|.png|.gif)$/i; //Extensiones permitidas
-    if(!allowefExtensions.exec(filePath)){ //El método exec() ejecuta una busqueda sobre las coincidencias de una expresión regular en una cadena especifica. Devuelve el resultado como array, o null.
-        let error = 'Carga un archivo de imagen válido, con las extensiones (.jpg - .jpeg - .png - .gif)'
-        fileError.innerHTML = error;
-        file.value = '';
-        document.getElementById('preview').innerHTML = '';
-        return false;
-    }else{
-        // Image preview
-        if(file.files && file.files[0]){
-            var reader = new FileReader();
-            reader.onload = function(e){
-                document.getElementById('preview').innerHTML = '<img src="' + e.target.result +'"/>';
-            };
-            reader.readAsDataURL(file.files[0]);
-            errorImage.innerHTML = '';
-        }
-    }
-})*/
--- public/js/registerValidatorFront.js
+++ public/js/registerValidatorFront.js
@@ -22,15 +22,11 @@ window.addEventListener("load", function () {
     switch (true) {
       case !$inputfirstName.value.trim():
         $firstNameErrors.innerHTML = "Debes escribir tu nombre";
-        $inputfirstName.classList.add("is-invalid");
         break;
       case !regExAlpha.test($inputfirstName.value):
         $firstNameErrors.innerHTML = "Debes ingresar un nombre válido";
-        $inputfirstName.classList.add("is-invalid");
         break;
       default:
-        $inputfirstName.classList.remove("is-invalid");
-        $inputfirstName.classList.add("is-valid");
         $firstNameErrors.innerHTML = "";
         break;
     }
@@ -41,15 +37,11 @@ window.addEventListener("load", function () {
     switch (true) {
       case !$inputlastName.value.trim():
         $lastNameErrors.innerHTML = "Debes escribir tu apellido";
-        $inputlastName.classList.add("is-invalid");
         break;
       case !regExAlpha.test($inputlastName.value):
         $lastNameErrors.innerHTML = "Debes ingresar un apellido válido";
-        $inputlastName.classList.add("is-invalid");
         break;
       default:
-        $inputlastName.classList.remove("is-invalid");
-        $inputlastName.classList.add("is-valid");
         $lastNameErrors.innerHTML = "";
         break;
     }
@@ -59,15 +51,11 @@ window.addEventListener("load", function () {
     switch (true) {
       case !$email.value.trim():
         $emailErrors.innerHTML = "El campo email es obligatorio";
-        $email.classList.add("is-invalid");
         break;
       case !regExEmail.test($email.value):
         $emailErrors.innerHTML = "Debe ingresar un email válido";
-        $email.classList.add("is-invalid");
         break;
       default:
-        $email.classList.remove("is-invalid");
-        $email.classList.add("is-valid");
         $emailErrors.innerHTML = "";
         break;
     }
@@ -77,16 +65,12 @@ window.addEventListener("load", function () {
     switch (true) {
       case !$password1.value.trim():
         $password1Errors.innerHTML = "El campo contraseña es obligatorio";
-        $password1.classList.add("is-invalid");
         break;
       case !regExPass.test($password1.value):
         $password1Errors.innerHTML =
           "La contraseña debe tener: entre 6 o 12 caracteres, al menos una mayúscula, una minúscula y un número";
-        $password1.classList.add("is-invalid");
         break;
       default:
-        $password1.classList.remove("is-invalid");
-        $password1.classList.add("is-valid");
         $password1Errors.innerHTML = "";
         break;
     }
@@ -96,15 +80,11 @@ window.addEventListener("load", function () {
     switch (true) {
       case !$password2.value.trim():
         $password2Errors.innerHTML = "Debes reingresar la contraseña";
-        $password2.classList.add("is-invalid");
         break;
       case $password2.value != $password1.value:
         $password2Errors.innerHTML = "Las contraseñas no coinciden";
-        $password2.classList.add("is-invalid");
         break;
       default:
-        $password2.classList.remove("is-invalid");
-        $password2.classList.add("is-valid");
         $password2Errors.innerHTML = "";
         break;
     }
@@ -118,7 +98,7 @@ window.addEventListener("load", function () {
 
     for (let index = 0; index < elementForm.length - 1; index++) {
       if (elementForm[index].value == "") {
-        elementForm[index].classList.add("is-invalid");
+        elementForm[index].style.boxShadow = '1px solid darkgray'
         submitErrors.innerHTML = "Los campos señalados son obligatorios";
         error = true;
       }
