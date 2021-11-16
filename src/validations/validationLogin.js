3b0192c22a4e8e0ef79fe1a417c65dcf7d36d16a
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2021-11-16 20:18:44 -0300

    validacion de login-back

--- src/validations/validationLogin.js
+++ src/validations/validationLogin.js
@@ -3,48 +3,41 @@ let bcrypt = require("bcryptjs");
 const db = require("../database/models");
 
 module.exports = [
-    check('emailLog')
+  check("emailLog")
     .notEmpty()
-    .withMessage('Debes ingresar un email').bail().isEmail()
-    .withMessage('Debes ingresar un email válido'),
-    body('emailLog')
-    .custom((value, { req }) => {
-        return db.User.findOne({
-                where: {
-                    email: req.body.emailLog
-                }
-            })
-            .then((email) => {
-                if (email) {
-                    return true
+    .withMessage("Debes ingresar un email")
+    .bail()
+    .isEmail()
+    .withMessage("Debes ingresar un email válido"),
+  body("emailLog").custom((value, { req }) => {
+    return db.User.findOne({
+      where: {
+        email: req.body.emailLog,
+      },
+    }).then((email) => {
+      if (email) {
+        return true;
+      } else {
+        return Promise.reject("Este correo aún no ha sido registrado");
+      }
+    });
+  }),
 
-                } else {
-                    return Promise.reject("Este correo aún no ha sido registrado")
-                }
-            })
+  check("passLog").notEmpty().withMessage("Debes ingresar una contraseña"),
 
-    }),
-
-
-    check('passLog')
-    .notEmpty()
-    .withMessage('Debes ingresar una contraseña'),
-
-    body('passLog')
-    .custom((value, { req }) => {
-        return db.User.findOne({
-                where: {
-                    email: req.body.emailLog
-                }
-            })
-            .then(user => {
-                if (!bcrypt.compareSync(req.body.passLog, user.dataValues.password)) {
-                    return Promise.reject()
-                }
-            })
-            .catch((err) => {
-                return Promise.reject("Email o contraseña incorrectos")
-            })
+  body("passLog").custom((value, { req }) => {
+    return db.User.findOne({
+      where: {
+        email: req.body.emailLog,
+      },
     })
-
-]
+      .then((user) => {
+        if (!bcrypt.compareSync(req.body.passLog, user.dataValues.password)) {
+          return Promise.reject();
+        }
+      })
+      .catch((err) => {
+        return Promise.reject("Email o contraseña incorrectos");
+      });
+  }),
+];
